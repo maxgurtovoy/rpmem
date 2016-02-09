70f12dc76717c688bc1965ca2cbef5a9097ff1b2
Max Gurtovoy <maxg@mellanox.com>
2016-02-09 10:29:37 +0200

    rpmem: initial commit
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

---
+++ rpmem.c
@@ -0,0 +1,361 @@
+/*
+ * Copyright (c) 2016 Mellanox Technologies.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+#include "rpmem.h"
+
+static void rpmem_handle_wc(struct ibv_wc *wc,
+			    struct mlx_rpmem_file *mlx_rfile)
+{
+	printf("file %p handle %p wc\n", mlx_rfile, wc);
+}
+
+static int cq_event_handler(struct mlx_rpmem_file *mlx_rfile)
+{
+	struct ibv_wc wc[16];
+	unsigned int i;
+	unsigned int n;
+	unsigned int completed = 0;
+
+	while ((n = ibv_poll_cq(mlx_rfile->cq, 16, wc)) > 0) {
+		for (i = 0; i < n; i++)
+			rpmem_handle_wc(&wc[i], mlx_rfile);
+
+		completed += n;
+		if (completed >= 64)
+			break;
+	}
+
+        if (n) {
+                perror("poll error");
+                return -1;
+        }
+
+        return 0;
+}
+
+static void *cq_thread(void *arg)
+{
+	struct mlx_rpmem_file *mlx_rfile = arg;
+        struct ibv_cq *ev_cq;
+        void *ev_ctx;
+        int ret;
+
+        while (1) {
+                pthread_testcancel();
+
+                ret = ibv_get_cq_event(mlx_rfile->comp_channel, &ev_cq, &ev_ctx);
+                if (ret) {
+                        perror("Failed to get cq event!");
+                        pthread_exit(NULL);
+                }
+
+                if (ev_cq != mlx_rfile->cq) {
+                        perror("Unknown CQ!");
+                        pthread_exit(NULL);
+                }
+
+                ret = ibv_req_notify_cq(mlx_rfile->cq, 0);
+                if (ret) {
+                        perror("Failed to set notify!");
+                        pthread_exit(NULL);
+                }
+
+                ret = cq_event_handler(mlx_rfile);
+
+                ibv_ack_cq_events(mlx_rfile->cq, 1);
+                if (ret)
+                        pthread_exit(NULL);
+        }
+}
+
+static int rpmem_addr_handler(struct rdma_cm_id *cma_id)
+{
+	struct mlx_rpmem_file *mlx_rfile = cma_id->context;
+        int ret, flags, max_cqe;
+
+        ret = rdma_resolve_route(cma_id, 1000);
+        if (ret) {
+		perror("rdma_resolve_route");
+		goto error;
+        }
+
+        mlx_rfile->pd = ibv_alloc_pd(cma_id->verbs);
+        if (!mlx_rfile->pd) {
+		perror("ibv_alloc_pd");
+		goto error;
+        }
+
+        mlx_rfile->comp_channel = ibv_create_comp_channel(cma_id->verbs);
+	if (!mlx_rfile->comp_channel) {
+		perror("ibv_create_comp_channel failed");
+		goto pd_error;
+	}
+
+	/* TODO: check if need to change to nonblock
+	flags = fcntl(mlx_rfile->comp_channel->fd, F_GETFL);
+	ret = fcntl(mlx_rfile->comp_channel->fd, F_SETFL, flags | O_NONBLOCK);
+	if (ret) {
+		perror("Failed to set channel to nonblocking");
+		goto comp_error;
+	}
+	*/
+
+        max_cqe = 8;
+
+        mlx_rfile->cq = ibv_create_cq(cma_id->verbs,
+				      max_cqe,
+				      mlx_rfile,
+				      mlx_rfile->comp_channel,
+				      0);
+        if (!mlx_rfile->cq) {
+		perror("Failed to create cq");
+		goto comp_error;
+	}
+
+        if (ibv_req_notify_cq(mlx_rfile->cq, 0)) {
+		perror("ibv_req_notify_cq failed");
+		goto cq_error;
+	}
+
+	pthread_create(&mlx_rfile->cqthread, NULL, cq_thread, mlx_rfile);
+
+        return 0;
+
+cq_error:
+        ibv_destroy_cq(mlx_rfile->cq);
+comp_error:
+	ibv_destroy_comp_channel(mlx_rfile->comp_channel);
+pd_error:
+        ibv_dealloc_pd(mlx_rfile->pd);
+error:
+	sem_post(&mlx_rfile->sem_connect);
+        return -1;
+}
+
+static int rpmem_route_handler(struct rdma_cm_id *cma_id)
+{
+	struct mlx_rpmem_file *mlx_rfile = (struct mlx_rpmem_file *)cma_id->context;
+        struct rdma_conn_param conn_param;
+	struct ibv_qp_init_attr init_attr;
+        int ret;
+
+	printf("rfile %p rpmem_route_handler cma_ctx %p id %p\n", mlx_rfile, cma_id->context, mlx_rfile->cma_id);
+
+        memset(&init_attr, 0, sizeof(init_attr));
+        init_attr.qp_context  = (void *) mlx_rfile->cma_id->context;
+        init_attr.send_cq     = mlx_rfile->cq;
+        init_attr.recv_cq     = mlx_rfile->cq;
+        init_attr.cap.max_send_wr  = 16;
+        init_attr.cap.max_recv_wr  = 16;
+        init_attr.cap.max_send_sge = 2;
+        init_attr.cap.max_recv_sge = 1;
+        init_attr.qp_type     = IBV_QPT_RC;
+
+        ret = rdma_create_qp(mlx_rfile->cma_id, mlx_rfile->pd, &init_attr);
+	if (ret) {
+		perror("rdma_create_qp");
+		return -1;
+	}
+
+        mlx_rfile->qp = mlx_rfile->cma_id->qp;
+
+        memset(&conn_param, 0, sizeof(struct rdma_conn_param));
+        conn_param.responder_resources = 1;    // Need to be attribute of device 
+        conn_param.initiator_depth     = 1;
+        conn_param.retry_count         = 7;
+        conn_param.rnr_retry_count     = 7;
+
+        ret = rdma_connect(cma_id, &conn_param);
+        if (ret) {
+		perror("rdma_connect");
+		goto destroy_qp;
+        }
+        return ret;
+
+destroy_qp:
+        ibv_destroy_qp(mlx_rfile->qp);
+
+        return -1;
+}
+
+static int rpmem_cma_handler(struct rdma_cm_id *cma_id,
+			     struct rdma_cm_event *event)
+{
+	struct mlx_rpmem_file *mlx_rfile = cma_id->context;
+        int ret = 0;
+
+	printf("rfile %p cma_event type %s cma_id %p\n", mlx_rfile, rdma_event_str(event->event), cma_id);
+
+	pthread_mutex_lock(&mlx_rfile->state_mutex);
+        switch(event->event) {
+		case RDMA_CM_EVENT_ADDR_RESOLVED:
+			ret = rpmem_addr_handler(cma_id);
+                        break;
+                case RDMA_CM_EVENT_ROUTE_RESOLVED:
+                        ret = rpmem_route_handler(cma_id);
+                        break;
+                case RDMA_CM_EVENT_ESTABLISHED:
+			mlx_rfile->state = RPMEM_ESTABLISHED;
+			sem_post(&mlx_rfile->sem_connect);
+                        break;
+                case RDMA_CM_EVENT_ADDR_ERROR:
+                case RDMA_CM_EVENT_ROUTE_ERROR:
+                case RDMA_CM_EVENT_CONNECT_ERROR:
+                case RDMA_CM_EVENT_UNREACHABLE:
+                case RDMA_CM_EVENT_REJECTED:
+			mlx_rfile->state = RPMEM_ERROR;
+                        ret = -1;
+			sem_post(&mlx_rfile->sem_connect);
+                        break;
+                case RDMA_CM_EVENT_DISCONNECTED:
+                case RDMA_CM_EVENT_ADDR_CHANGE:
+                case RDMA_CM_EVENT_TIMEWAIT_EXIT:
+			mlx_rfile->state = RPMEM_DISCONNECTED;
+			ret = -1;
+			sem_post(&mlx_rfile->sem_connect);
+                        break;
+                default:
+			mlx_rfile->state = RPMEM_ERROR;
+                        ret = -1;
+			sem_post(&mlx_rfile->sem_connect);
+                        break;
+        }
+	pthread_mutex_unlock(&mlx_rfile->state_mutex);
+
+        return ret;
+}
+
+static void *cm_thread(void *arg)
+{
+	struct mlx_rpmem_file *mlx_rfile = arg;
+        struct rdma_cm_event *event;
+        struct rdma_cm_event event_copy;
+        int ret;
+
+        while (1) {
+		ret = rdma_get_cm_event(mlx_rfile->cma_channel, &event);
+		if (ret) {
+			perror("Failed to get RDMA-CM Event");
+			pthread_exit(NULL);
+		}
+		printf("rfile %p event %p\n", mlx_rfile, event);
+             	memcpy(&event_copy, event, sizeof(struct rdma_cm_event));
+		ret = rpmem_cma_handler(mlx_rfile->cma_id, &event_copy);
+		if (ret) {
+			perror("Failed to handle event");
+			pthread_exit(NULL);
+		}
+		rdma_ack_cm_event(event);
+	}
+}
+
+struct rpmem_file *
+rpmem_open(struct sockaddr *dst_addr, char *rfilepath, int flags)
+{
+	struct mlx_rpmem_file *mlx_rfile;
+	struct rpmem_file *rfile;
+	int ret;
+
+	mlx_rfile = calloc(1, sizeof(struct mlx_rpmem_file));
+	if (!mlx_rfile) {
+		perror("No Memory");
+		return NULL;
+	}
+
+	rfile = &mlx_rfile->rfile;
+	pthread_mutex_init(&mlx_rfile->state_mutex, NULL);
+	sem_init(&mlx_rfile->sem_connect, 0, 0);
+
+	mlx_rfile->cma_channel = rdma_create_event_channel();
+	if (!mlx_rfile->cma_channel) {
+		perror("rdma_create_event_channel");
+		goto destroy_rfile;
+	}
+
+	printf("rfile %p after rdma_create_event_channel\n", mlx_rfile);
+
+	if (rdma_create_id(mlx_rfile->cma_channel, &mlx_rfile->cma_id, mlx_rfile, RDMA_PS_TCP)) {
+		perror("rdma_create_id");
+		goto destroy_event_channel;
+	}
+
+	printf("rfile %p after rdma_create_id cma_ctx %p\n", mlx_rfile, mlx_rfile->cma_id->context);
+
+	ret = pthread_create(&mlx_rfile->cmthread, NULL, cm_thread, mlx_rfile);
+	if (ret) {
+		perror("Failed create Connection Manager Thread");
+		goto destroy_id;
+	}
+
+	printf("rfile %p after cm pthread_create cma_ctx %p\n", mlx_rfile, mlx_rfile->cma_id->context);
+
+	if (rdma_resolve_addr(mlx_rfile->cma_id, NULL, dst_addr, 2000)) {
+		perror("rdma_resolve_addr");
+		goto destroy_thread;
+	}
+
+	printf("rfile %p rdma_resolve_addr cma_ctx %p\n", mlx_rfile, mlx_rfile->cma_id->context);
+
+	sem_wait(&mlx_rfile->sem_connect);
+
+	printf("rfile %p sem_wait cma_ctx %p\n", mlx_rfile, mlx_rfile->cma_id->context);
+
+	pthread_mutex_lock(&mlx_rfile->state_mutex);
+	if (mlx_rfile->state != RPMEM_ESTABLISHED) {
+		perror("Conn Error");
+		pthread_mutex_unlock(&mlx_rfile->state_mutex);
+		goto destroy_thread;
+	}
+	pthread_mutex_unlock(&mlx_rfile->state_mutex);
+
+	/* RDMA connection established - need to open remote file */
+	rfile->rfilepath = strdup(rfilepath);
+	if (!rfile->rfilepath) {
+		perror("strdup");
+		goto destroy_thread;
+	}
+
+	return rfile;
+
+destroy_thread:
+	pthread_cancel(mlx_rfile->cmthread);
+destroy_id:
+	rdma_destroy_id(mlx_rfile->cma_id);
+destroy_event_channel:
+	rdma_destroy_event_channel(mlx_rfile->cma_channel);
+destroy_rfile:
+	free(mlx_rfile);
+
+	return NULL;
+}
+
---
+++ rpmem.h
@@ -0,0 +1,141 @@
+/*
+ * Copyright (c) 2016 Mellanox Technologies.  All rights reserved.
+ *
+ * This software is available to you under a choice of one of two
+ * licenses.  You may choose to be licensed under the terms of the GNU
+ * General Public License (GPL) Version 2, available from the file
+ * COPYING in the main directory of this source tree, or the
+ * OpenIB.org BSD license below:
+ *
+ *     Redistribution and use in source and binary forms, with or
+ *     without modification, are permitted provided that the following
+ *     conditions are met:
+ *
+ *      - Redistributions of source code must retain the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer.
+ *
+ *      - Redistributions in binary form must reproduce the above
+ *        copyright notice, this list of conditions and the following
+ *        disclaimer in the documentation and/or other materials
+ *        provided with the distribution.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef RPMEM_H
+#define RPMEM_H
+
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#include <infiniband/verbs.h>
+#include <rdma/rdma_cma.h>
+#include <rdma/rdma_verbs.h>
+
+
+enum file_state {
+	RPMEM_INIT,
+	RPMEM_ERROR,
+	RPMEM_ESTABLISHED,
+	RPMEM_DISCONNECTED
+};
+
+struct rpmem_file {
+	char				*rfilepath;
+	struct sockaddr			dst_addr;
+	int				fd;
+};
+
+struct mlx_rpmem_file {
+	struct rpmem_file		rfile;
+
+	struct rdma_cm_id		*cma_id;
+	struct rdma_event_channel	*cma_channel;
+
+	struct ibv_device		*ib_device;
+	struct ibv_device_attr        	dev_attr;
+	struct ibv_pd                	*pd;
+	struct ibv_cq                	*cq;
+	struct ibv_qp                	*qp;
+	struct ibv_comp_channel      	*comp_channel;
+
+	sem_t	                        sem_connect;
+	pthread_t	                cmthread;
+	pthread_t	                cqthread;
+
+	enum file_state         	state;
+	pthread_mutex_t 		state_mutex;
+	/* information of remote persistance */
+	//struct list_head		rmap_list;
+	//int				nrmaps;
+};
+
+struct rpmem_mr {
+	void		*addr;
+	size_t		len;
+};
+
+struct mlx_rpmem_mr {
+	struct rpmem_mr		rmr;
+	uint32_t		rkey;
+};
+
+struct rpmem_sge {
+	off_t			addr;
+	size_t			len;
+};
+
+/*
+struct rpmem_comp {
+	void (*done)(struct struct rpmem_comp *comp);
+	bool persistent;
+};
+*/
+
+struct rpmem_file *
+rpmem_open(struct sockaddr *dst_addr, char *rfilepath, int flags);
+
+struct rpmem_mr *
+rpmem_map(struct rpmem_file *file,
+	  off_t offset, size_t len);
+
+void
+rpmem_unmap(struct rpmem_mr *mr);
+
+struct rpmem_mr *
+rpmem_map_shadow(struct rpmem_file *file,
+		 off_t offset, void *addr, size_t len);
+
+int rpmem_close(struct rpmem_file *);
+
+/* blocking */
+int rpmem_flush_sync(struct rpmem_mr *mr, struct rpmem_sge *sges, int nr_sges);
+
+/* non-blocking */
+/*
+int rpmem_flush_async(struct rpmem_sge *sges, int nr_sges,
+		      struct rpmem_comp *comp);
+*/
+/* drains rdma all completions on mr */
+int rpmem_drain(struct rpmem_mr *mr);
+
+/* flush async and drain all rpmems */
+int rpmem_persist_sync(struct rpmem_sge *sges, int nr_sges);
+
+/* flush async and drain all rpmems */
+/*
+int rpmem_persist_async(struct rpmem_sge *sges, int nr_sges,
+			 struct rpmem_comp *comp);
+*/
+
+#endif /* RPMEM_H */
