d6a1a45462f41a2bc43e4906517330a4908de661
Max Gurtovoy <maxg@mellanox.com>
2016-04-10 01:23:49 +0300

    add offsetof and container_of defines
    Signed-off-by: Max Gurtovoy <maxg@mellanox.com>

--- rpmem_common.h
+++ rpmem_common.h
@@ -47,6 +47,24 @@
 
 #include "rpmem_protocol.h"
 
+#ifndef offsetof
+#define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
+#endif
+
+/**
+ * container_of - cast a member of a structure out to the containing structure
+ *
+ * @ptr:        the pointer to the member.
+ * @type:       the type of the container struct this is embedded in.
+ * @member:     the name of the member within the struct.
+ *
+ */
+#ifndef container_of
+#define container_of(ptr, type, member) ({			\
+	const typeof(((type *)0)->member)*__mptr = (ptr);	\
+	(type *)((char *)__mptr - offsetof(type, member)); })
+#endif
+
 #ifndef SLIST_FOREACH_SAFE
 #define SLIST_FOREACH_SAFE(var, head, field, tvar)                       \
         for ((var) = SLIST_FIRST((head));                                \
